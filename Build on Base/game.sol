2eb1c264507e6a87e459496938212058a21dfebe
Emmanuel-webDev <emmanuelonuorah875@gmail.com>
2025-10-27 21:37:28 +0100

    Setting up contract

---
+++ Build on Base/game.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+//Simple guessing game with leaderboard. leaderboard displays user address and correct number of guesses, also displays the current logged in users detials on the leaderboard
+
+contract GuessGame{
+
+}
