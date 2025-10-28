7949e72b1058c1ffd8a133b2aa5479e581b6c586
Emmanuel-webDev <emmanuelonuorah875@gmail.com>
2025-10-28 11:20:30 +0100

    Created the play function

--- Build on Base/game.sol
+++ Build on Base/game.sol
@@ -4,8 +4,6 @@ pragma solidity 0.8.30;
 //Simple guessing game with leaderboard. leaderboard displays user address and correct number of guesses, also displays the current logged in users detials on the leaderboard
 
 contract GuessGame{
-    //Value user Guessed
-    uint public num;
 
     //Generated value
     uint public generatedNum;
@@ -13,18 +11,39 @@ contract GuessGame{
     //Stores a user data
         struct User{
             address user;
+            bool isRegistered;
             uint guessCount;
             uint amountWon;
         }
+
+
     //Maps current user address to it's data
         mapping(address => User) public usersInfo;
+
+
+    //Array to hold all users
+    User[] public allPlayers;
 
     function generateNum() public{
         uint randomNum = uint(keccak256(abi.encodePacked(block.timestamp,block.prevrandao,msg.sender)));
         generatedNum = (randomNum % 100);
     }    
 
-    function check() public {
+    function play(uint num) public{
+
+     // If first time playing → register user once
+    if (!usersInfo[msg.sender].isRegistered) {
+        usersInfo[msg.sender].user = msg.sender;
+        usersInfo[msg.sender].isRegistered = true;
+        allPlayers.push(usersInfo[msg.sender]);
+    }
+
+      usersInfo[msg.sender].guessCount += 1;
+     
+     if(num == generatedNum){
+        usersInfo[msg.sender].amountWon += 1;
+     }
+    
 
     }
 }
