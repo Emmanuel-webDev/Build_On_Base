444feb8a4bc1ba23d683ec52b3d9cca50182156e
Emmanuel-webDev <emmanuelonuorah875@gmail.com>
2025-10-28 12:15:47 +0100

    Updated Play function and handled wrong guess error

--- Build on Base/game.sol
+++ Build on Base/game.sol
@@ -3,47 +3,50 @@ pragma solidity 0.8.30;
 
 //Simple guessing game with leaderboard. leaderboard displays user address and correct number of guesses, also displays the current logged in users detials on the leaderboard
 
-contract GuessGame{
-
+contract GuessGame {
     //Generated value
     uint public generatedNum;
 
     //Stores a user data
-        struct User{
-            address user;
-            bool isRegistered;
-            uint guessCount;
-            uint amountWon;
-        }
-
-
-    //Maps current user address to it's data
-        mapping(address => User) public usersInfo;
-
-
-    //Array to hold all users
-    User[] public allPlayers;
-
-    function generateNum() public{
-        uint randomNum = uint(keccak256(abi.encodePacked(block.timestamp,block.prevrandao,msg.sender)));
-        generatedNum = (randomNum % 100);
-    }    
-
-    function play(uint num) public{
-
-     // If first time playing → register user once
-    if (!usersInfo[msg.sender].isRegistered) {
-        usersInfo[msg.sender].user = msg.sender;
-        usersInfo[msg.sender].isRegistered = true;
-        allPlayers.push(usersInfo[msg.sender]);
+    struct User {
+        address user;
+        bool isRegistered;
+        uint guessCount;
+        uint amountWon;
     }
 
-      usersInfo[msg.sender].guessCount += 1;
-     
-     if(num == generatedNum){
-        usersInfo[msg.sender].amountWon += 1;
-     }
-    
+    //Maps current user address to it's data
+    mapping(address => User) public usersInfo;
 
+    //Array to hold all users
+    address[] public allPlayers;
+
+    //Event to handle wrong guess 
+    event GuessFailed(uint guess, uint answer);
+
+    function generateNum() public {
+        uint randomNum = uint(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
+            )
+        );
+        generatedNum = (randomNum % 100);
+    }
+
+    function play(uint num) public {
+        // If first time playing → register user once
+        if (!usersInfo[msg.sender].isRegistered) {
+            usersInfo[msg.sender].user = msg.sender;
+            usersInfo[msg.sender].isRegistered = true;
+            allPlayers.push(msg.sender);
+        }
+
+        usersInfo[msg.sender].guessCount += 1;
+
+        if (num == generatedNum) {
+            usersInfo[msg.sender].amountWon += 1;
+        }else{
+            emit GuessFailed(num, generatedNum);
+        }
     }
 }
