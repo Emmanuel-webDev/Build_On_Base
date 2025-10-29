b81eb87a693c82a331a89951be9a1086c61d9f0c
Emmanuel-webDev <emmanuelonuorah875@gmail.com>
2025-10-29 10:40:42 +0100

    Modified the play function so that the generated number gets updated after each play

--- Build on Base/game.sol
+++ Build on Base/game.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.30;
 //Simple guessing game with leaderboard. leaderboard displays user address and correct number of guesses, also displays the current logged in users detials on the leaderboard
 
 contract GuessGame {
-    //Generated value
+    //Generated Value
     uint public generatedNum;
 
     //Stores a user data
@@ -21,17 +21,8 @@ contract GuessGame {
     //Array to hold all users
     address[] public allPlayers;
 
-    //Event to handle wrong guess 
+    //Event to handle wrong guess
     event GuessFailed(uint guess, uint answer);
-
-    function generateNum() public {
-        uint randomNum = uint(
-            keccak256(
-                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
-            )
-        );
-        generatedNum = (randomNum % 100);
-    }
 
     function play(uint num) public {
         // If first time playing → register user once
@@ -43,27 +34,42 @@ contract GuessGame {
 
         usersInfo[msg.sender].guessCount += 1;
 
+        uint randomNum = uint(
+            keccak256(
+                abi.encodePacked(block.timestamp, block.prevrandao, msg.sender)
+            )
+        );
+
+        //Generatd value
+        generatedNum = (randomNum % 100);
+
         if (num == generatedNum) {
             usersInfo[msg.sender].amountWon += 1;
-        }else{
+        } else {
             emit GuessFailed(num, generatedNum);
         }
     }
 
-    function leaderBoard() public view returns (address[] memory players, uint[] memory guesses, uint[] memory corrects) {
-    uint length = allPlayers.length;
+    function leaderBoard()
+        public
+        view
+        returns (
+            address[] memory players,
+            uint[] memory guesses,
+            uint[] memory corrects
+        )
+    {
+        uint length = allPlayers.length;
 
-    players = new address[](length);
-    guesses = new uint[](length);
-    corrects = new uint[](length);
+        players = new address[](length);
+        guesses = new uint[](length);
+        corrects = new uint[](length);
 
-    for (uint i = 0; i < length; i++) {
-        address playerAddr = allPlayers[i];
-        players[i] = playerAddr;
-        guesses[i] = usersInfo[playerAddr].guessCount;
-        corrects[i] = usersInfo[playerAddr].amountWon;
+        for (uint i = 0; i < length; i++) {
+            address playerAddr = allPlayers[i];
+            players[i] = playerAddr;
+            guesses[i] = usersInfo[playerAddr].guessCount;
+            corrects[i] = usersInfo[playerAddr].amountWon;
+        }
     }
-
-}
-
 }
