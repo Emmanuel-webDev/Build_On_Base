a04c5575d444d67189718f7490eb77161c3a9f8a
Chinonso <ceothedev18@gmail.com>
2025-11-09 19:36:20 +0100

    updated miniapp sdk config to enable farcaster wallet connection

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import {connectWalletConnect} from "./wallet.js";
-import { sdk } from "https://esm.sh/@farcaster/miniapp-sdk";
+//import {connectWalletConnect} from "./wallet.js";
+//import { sdk } from "https://esm.sh/@farcaster/miniapp-sdk";
 
 const contractAddress = "0x6F8Bf9b227da8c2bA64125Cbf15aDC85B1F6AF4B"; // Contract address
 
@@ -181,7 +181,21 @@ document.getElementById("connect").onclick = async function init() {
         }
       }
     } else {
-      provider = await connectWalletConnect();
+      //     provider = await connectWalletConnect();
+
+      if (window.farcaster?.miniapp?.ethereum) {
+        const provider = new ethers.providers.Web3Provider(
+          window.farcaster.miniapp.ethereum,
+          "any"
+        );
+        await provider.send("eth_requestAccounts", []);
+        let sign = provider.getSigner();
+        const addy = await sign.getAddress();
+        console.log("Connected Address:", addy);
+        return;
+      } else {
+        console.log("Fallback to WalletConnect or MetaMask");
+      }
     }
 
     signer = provider.getSigner(); //account that is connected
